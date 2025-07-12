72d876c78b9032122a6c9fbcd6881f212651739e
Vladimir Cortés A. <vladimir.cortes@outlook.com>
2025-07-12 11:17:27 -0500

    feat(frontend): Adicione SecureStringToStringConverter

---
+++ GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/Converters/SecureStringToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GestionVoluntariadoEventosGUI.Converters
+{
+    public class SecureStringToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SecureString secureString)
+            {
+                return new System.Net.NetworkCredential(string.Empty, secureString).Password;
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();//Pendiente
+        }
+    }
+}
