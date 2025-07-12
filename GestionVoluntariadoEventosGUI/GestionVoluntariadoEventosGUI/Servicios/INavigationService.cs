1233db8a17edd1674887bfa88be70f1d43a43e22
Vladimir Cortés A. <vladimir.cortes@outlook.com>
2025-07-12 12:07:42 -0500

    feat(frontend): Adicione INavigationService y NavigationService

---
+++ GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/Servicios/INavigationService.cs
@@ -0,0 +1,15 @@
+﻿using GestionVoluntariadoEventosGUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionVoluntariadoEventosGUI.Servicios
+{
+    public interface INavigationService
+    {
+        void NavigateTo(BaseViewModel viewModel);
+        void GoBack();
+    }
+}
---
+++ GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/Servicios/NavigationService.cs
@@ -0,0 +1,22 @@
+﻿using GestionVoluntariadoEventosGUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionVoluntariadoEventosGUI.Servicios
+{
+    public class NavigationService : INavigationService
+    {
+        public void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NavigateTo(BaseViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
