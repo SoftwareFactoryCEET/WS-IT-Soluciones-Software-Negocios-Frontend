8b7b5fd7fea315a6558710c16bf624f699fd13e0
Vladimir Cortés A. <vladimir.cortes@outlook.com>
2025-07-12 10:46:28 -0500

    feat(frontend): Adicione modelos Event, User y LoginRequest.

---
+++ GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionVoluntariadoEventosGUI.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int DurationMinutes { get; set; }
+        public string? SpecialRequirements { get; set; }
+        public int VolunteersRequired { get; set; }
+        public string OrganizerContact { get; set; } = string.Empty;
+    }
+}
