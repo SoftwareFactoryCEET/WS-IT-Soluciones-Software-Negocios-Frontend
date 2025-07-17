70c44ddbaf664eedc6fe8d77d9b4ea77b4fe8180
Vladimir Cortés A. <vladimir.cortes@outlook.com>
2025-07-16 21:09:10 -0500

    feat(frontend): Adicione MainMenuViewModel

--- GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/ViewModels/MainMenuViewModel.cs
+++ GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/ViewModels/MainMenuViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using GestionVoluntariadoEventosGUI.Commands;
+using GestionVoluntariadoEventosGUI.Servicios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace GestionVoluntariadoEventosGUI.ViewModels
 {
-    public class MainMenuViewModel
+    public class MainMenuViewModel : BaseViewModel
     {
+        private readonly ApiService _apiService;
+        private readonly NavigationService _navigationService;
+
+        // Propiedades y comandos para la pantalla principal irán aquí
+        // Por ejemplo, comandos para navegar a la creación de eventos, ver voluntarios, etc.
+        public ICommand NavigateToEventCreationCommand { get; }
+        public ICommand LogoutCommand { get; }
+
+        public MainMenuViewModel(ApiService apiService, NavigationService navigationService)
+        {
+            _apiService = apiService;
+            _navigationService = navigationService;
+            // Inicializar comandos
+            NavigateToEventCreationCommand = new RelayCommand((p) => NavigateToEventCreation());
+            LogoutCommand = new RelayCommand((p) => Logout());
+        }       
+
+        private void NavigateToEventCreation()
+        {
+            //_navigationService.NavigateTo(new EventCreationViewModel(_apiService, _navigationService));
+        }
+
+        private void Logout()
+        {
+            // Lógica para cerrar sesión (por ejemplo, limpiar datos de usuario, volver a la pantalla de login)
+            _navigationService.NavigateTo(new LoginViewModel(_apiService, _navigationService));
+        }
     }
 }
