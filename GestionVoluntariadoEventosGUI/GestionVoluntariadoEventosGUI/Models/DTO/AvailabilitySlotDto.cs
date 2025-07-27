04b2c5d0232a269cd841e24c8e458951eeddd27c
Vladimir Cortés A. <vladimir.cortes@outlook.com>
2025-07-27 16:42:53 -0500

    feat(frontend): adicione AvailabilitySlotDto.cs y VolunteerDto.cs

---
+++ GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/Models/DTO/AvailabilitySlotDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GestionVoluntariadoEventosGUI.Models.DTO
+{
+    public class AvailabilitySlotDto
+    {
+        public string DayOfWeek { get; set; } = string.Empty;
+        public TimeOnly StartTime { get; set; } // <-- ¡TimeOnly aquí!
+        public TimeOnly EndTime { get; set; }   // <-- ¡TimeOnly aquí!
+    }
+}
---
+++ GestionVoluntariadoEventosGUI/GestionVoluntariadoEventosGUI/Models/DTO/VolunteerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionVoluntariadoEventosGUI.Models.DTO
+{
+    public class VolunteerDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; }
+        public string Skills { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public List<AvailabilitySlotDto> AvailabilitySlots { get; set; } = new List<AvailabilitySlotDto>();
+    }
+}
